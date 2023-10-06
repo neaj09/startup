5faa255ef3a8940e1a2e5c33ebca8c3962156a91
neaj09 <iwayne5@live.fr>
2023-10-06 23:54:19 +0200

    modif url back

--- src/services/caller.service.js
+++ src/services/caller.service.js
@@ -4,8 +4,8 @@ import router from "@/router"
 
 // URL d'accès à mon serveur
 const Axios = axios.create({
-    baseURL: process.env.VUE_APP_API_URL
-    // baseURL: 'https://back-8hi1.onrender.com'
+    // baseURL: process.env.VUE_APP_API_URL
+    baseURL: 'https://back-8hi1.onrender.com'
 })
 
 // intercepte la requête et ajoute le token dans l'entête Authorization
